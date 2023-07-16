058f445315c207060df4c0db7b8ec8048a90fb77
Satheeshkumar Subramanian <jsatheesh568@gmail.com>
2023-07-16 11:34:34 +0530

    Feat: Add testcase to return the value to get all the five books

--- src/main/java/com/kata/developmentbookstore/model/BookInfo.java
+++ src/main/java/com/kata/developmentbookstore/model/BookInfo.java
@@ -5,7 +5,8 @@ public enum BookInfo {
 	CLEAN_CODE("Clean Code", "Robert Martin", 2008), 
 	CLEAN_CODER("Clean Coder", "Robert Martin", 2011),
 	CLEAN_ARCHITECTURE("Clean Architecture", "Robert Martin", 2017),
-	TDD_BY_EXAMPLE("Test Driven Development by Example", "Kent Beck", 2003);
+	TDD_BY_EXAMPLE("Test Driven Development by Example", "Kent Beck", 2003),
+	LEGACY_CODE("Working effectively with Legacy Code", "Michael C. Feathers", 2004);
 	
 	private String title;
 	private String author;
