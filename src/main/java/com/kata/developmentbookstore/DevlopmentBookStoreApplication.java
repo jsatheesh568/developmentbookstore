d17763d08f788b1d410ec680752fea78182177bb
Satheeshkumar Subramanian <jsatheesh568@gmail.com>
2023-07-16 08:13:25 +0530

    Feat: Developmentbookstore application project setup initial commit

---
+++ src/main/java/com/kata/developmentbookstore/DevlopmentBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.kata.developmentbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlopmentBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlopmentBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/developmentbookstore/DevlopmentBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.developmentbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlopmentBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
