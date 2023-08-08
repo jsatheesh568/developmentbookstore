bae40130e7d43a682b3a76248c78d994ca8123c4
jsatheesh568 <jsatheesh568@gmail.com>
2023-08-08 13:24:09 +0530

    Refactor: Refactor the swagger configuration and add lombok depedency

--- src/main/java/com/kata/developmentbookstore/config/SwaggerConfig.java
+++ src/main/java/com/kata/developmentbookstore/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package com.kata.developmentbookstore.config;
 
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -13,10 +15,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.kata.bookstore.controller")).paths(PathSelectors.any())
-				.build();
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kata.developmentbookstore.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
