fbf4d408ef81b6b2a4dfb2264014148a5e418699
jsatheesh568 <jsatheesh568@gmail.com>
2023-08-08 17:54:46 +0530

    Refactor: remove unused imports and add junit platform dependencies

--- src/test/java/com/kata/developmentbookstore/DevelopmentBookStoreServiceTests.java
+++ src/test/java/com/kata/developmentbookstore/DevelopmentBookStoreServiceTests.java
@@ -1,33 +1,24 @@
 package com.kata.developmentbookstore;
 
-import static org.mockito.Mockito.mock;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import com.kata.developmentbookstore.controller.BookController;
+import com.kata.developmentbookstore.exception.BookValidationException;
+import com.kata.developmentbookstore.model.Book;
+import com.kata.developmentbookstore.service.BookService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.BindingResult;
 
-import com.kata.developmentbookstore.controller.BookController;
-import com.kata.developmentbookstore.exception.BookValidationException;
-import com.kata.developmentbookstore.exception.EmptyCartException;
-import com.kata.developmentbookstore.model.Book;
-import com.kata.developmentbookstore.service.BookService;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
 
 
 class DevelopmentBookStoreServiceTests {
